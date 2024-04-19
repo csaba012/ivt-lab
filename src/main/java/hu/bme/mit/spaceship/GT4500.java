ced6f6900352511021e6a7f8730b65c6829e73df
csaba012 <papp.csaba28@gmail.com>
2024-04-19 13:28:11 +0200

    merge conflict branch-B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,6 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
+        // merge conflict line branch-B
         if (!primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
         }
